41990e2649113d71d70bd3fea90ad6f9f9268d74
xzpei <peixuze@shomop.com>
2024-10-10 15:20:48 +0800

    日志修改

--- lib/config.py
+++ lib/config.py
@@ -1,8 +1,10 @@
 import logging
+import sys
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(processName)s - %(threadName)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(processName)s - %(threadName)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)]
 )
 
 logger = logging.getLogger(__name__)
